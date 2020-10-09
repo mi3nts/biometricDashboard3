2d6ac1cb11986a66b0355ec4b927c26240a1eaf4
rohitkshenoy <36802469+rohitkshenoy@users.noreply.github.com>
2020-10-08 22:20:53 -0400

    Adding Bokeh Number visualization module
    Static only

---
+++ slides/meeting_6/bokehNumber.py
@@ -0,0 +1,31 @@
+import numpy as np
+import random as random
+from bokeh.io import curdoc, show
+from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Text
+
+N = 9
+x = np.linspace(0, 2, N)
+y = x
+text = [str(random.randint(0,N))]
+
+source = ColumnDataSource(dict(x=x, y=y, text=text))
+
+plot = Plot(
+    title=None, plot_width=300, plot_height=300,
+    min_border=0, toolbar_location=None)
+
+glyph = Text(x="x", y="y", text="text", text_font_size="130px", text_baseline=enumeration("middle"), angle=0, text_color="#96deb3")
+plot.add_glyph(source, glyph)
+
+xaxis = LinearAxis()
+plot.add_layout(xaxis, 'below')
+
+yaxis = LinearAxis()
+plot.add_layout(yaxis, 'left')
+
+plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
+plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+
+curdoc().add_root(plot)
+
+show(plot)
