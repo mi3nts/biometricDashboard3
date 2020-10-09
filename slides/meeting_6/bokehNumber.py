5dc1bbef8dfc7d08e4a6298c7cc03826ba451b08
rohitkshenoy <36802469+rohitkshenoy@users.noreply.github.com>
2020-10-09 02:02:08 -0400

    Meeting 6
    Slides + Python file

--- slides/meeting_6/bokehNumber.py
+++ slides/meeting_6/bokehNumber.py
@@ -1,31 +1,35 @@
 import numpy as np
 import random as random
 from bokeh.io import curdoc, show
+from bokeh.io.state import *
 from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Text
+from bokeh.core import enums
+from bokeh.core.properties import Enum
 
-N = 9
-x = np.linspace(0, 2, N)
-y = x
-text = [str(random.randint(0,N))]
+def clear_plot():
+    reset(plot)
+    '''source2 = ColumnDataSource(dict(x=[], y=[], text=[]))
+    glyph2 = Text(x="x", y="y", text="text", text_font_size="200px", text_align="center", text_baseline="middle",
+                 angle=0, text_color="#96deb3")
+    plot.add_glyph(source2, glyph2)
+    print("Cleared")'''
 
-source = ColumnDataSource(dict(x=x, y=y, text=text))
+def add_text():
+    N = 9
+    x = [1]
+    y = [1]
+    text = [str(random.randint(0, N))]
+    source = ColumnDataSource(dict(x=x, y=y, text=text))
+    glyph = Text(x="x", y="y", text="text", text_font_size="200px", text_align="center", text_baseline="middle", angle=0, text_color="#96deb3")
+    plot.add_glyph(source, glyph)
+    print("Printed " + str(text[0]))
 
 plot = Plot(
-    title=None, plot_width=300, plot_height=300,
-    min_border=0, toolbar_location=None)
-
-glyph = Text(x="x", y="y", text="text", text_font_size="130px", text_baseline=enumeration("middle"), angle=0, text_color="#96deb3")
-plot.add_glyph(source, glyph)
-
-xaxis = LinearAxis()
-plot.add_layout(xaxis, 'below')
-
-yaxis = LinearAxis()
-plot.add_layout(yaxis, 'left')
-
-plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
-plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+    title=None, plot_width=900, plot_height=600,
+    min_border=0, toolbar_location=None, reset_policy="standard")
 
 curdoc().add_root(plot)
+curdoc().add_periodic_callback(clear_plot, 2000)
+curdoc().add_periodic_callback(add_text, 2000)
 
 show(plot)
