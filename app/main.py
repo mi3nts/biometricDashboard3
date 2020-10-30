60559a1dba66dc0d2838fc8548889dd04db09089
arjunsridhar1234 <48868607+arjunsridhar1234@users.noreply.github.com>
2020-10-30 13:01:20 -0500

    EEG Update function added

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ from modules.respModule import respModule
 # import bokeh modules
 from bokeh.plotting import curdoc, figure
 from bokeh.models import Text, Plot
-from bokeh.layouts import column, row
+from bokeh.layouts import column, row, Spacer
 
 # # import other modules
 from functools import partial
@@ -38,9 +38,9 @@ inlet = getStream(0)
 # initialize QRS detector
 ecg_processing = QRSDetectorOnline()
 
-# CREATE BOKEH DATA STRUCTURE TO EFFICENTLY STORE AND UPDATE DATA
+# CREATE BOKEH DATA STRUCTURES TO EFFICENTLY STORE AND UPDATE DATA
 # ------------------------------------------------------------------------------
-source, source_num = mkColumnDataSources()
+source_eeg, source_ecg, source_num = mkColumnDataSources()
 
 # DEFINE DOCUMENT OBJECT
 # ------------------------------------------------------------------------------
@@ -48,21 +48,23 @@ source, source_num = mkColumnDataSources()
 # document.
 doc = curdoc()
 
-i=0 # initialize x index
+i=0 # initialize x index (current sample number)
+
+i_eeg = 0
 
 # DEFINE FUNCTIONS TO GET REAL TIME DATA
 # ------------------------------------------------------------------------------
+
 @gen.coroutine
-def update(eeg_1, eeg_2, eeg_3, eeg_4, eeg_5, eeg_6, eeg_7, eeg_8, eeg_9, eeg_10,\
+def update_eeg(eeg_1, eeg_2, eeg_3, eeg_4, eeg_5, eeg_6, eeg_7, eeg_8, eeg_9, eeg_10,\
 eeg_11, eeg_12, eeg_13, eeg_14, eeg_15, eeg_16,eeg_17, eeg_18, eeg_19, eeg_20, \
 eeg_21, eeg_22, eeg_23, eeg_24, eeg_25, eeg_26, eeg_27, eeg_28, eeg_29, eeg_30, \
 eeg_31, eeg_32, eeg_33, eeg_34, eeg_35, eeg_36, eeg_37, eeg_38, eeg_39, eeg_40,\
 eeg_41, eeg_42, eeg_43, eeg_44, eeg_45, eeg_46, eeg_47, eeg_48, eeg_49, eeg_50, \
 eeg_51, eeg_52, eeg_53, eeg_54, eeg_55, eeg_56, eeg_57, eeg_58, eeg_59, eeg_60, \
-eeg_61, eeg_62, eeg_63, eeg_64,\
-ecg_x, ecg_y):
+eeg_61, eeg_62, eeg_63, eeg_64):
 
-    source.stream(dict(eeg_1=[eeg_1], eeg_2=[eeg_2], \
+    source_eeg.stream(dict(eeg_1=[eeg_1], eeg_2=[eeg_2], \
     eeg_3=[eeg_3], eeg_4=[eeg_4], eeg_5=[eeg_5], eeg_6=[eeg_6], eeg_7=[eeg_7], \
     eeg_8=[eeg_8], eeg_9=[eeg_9], eeg_10=[eeg_10],eeg_11=[eeg_11], eeg_12=[eeg_12], \
     eeg_13=[eeg_13], eeg_14=[eeg_14], eeg_15=[eeg_15], eeg_16=[eeg_16], eeg_17=[eeg_17], \
@@ -75,9 +77,15 @@ ecg_x, ecg_y):
     eeg_48=[eeg_48], eeg_49=[eeg_49], eeg_50=[eeg_50], eeg_51=[eeg_51], eeg_52=[eeg_52],\
     eeg_53=[eeg_53], eeg_54=[eeg_54], eeg_55=[eeg_55], eeg_56=[eeg_56], eeg_57=[eeg_57], \
     eeg_58=[eeg_58], eeg_59=[eeg_59], eeg_60=[eeg_60], eeg_61=[eeg_61], eeg_62=[eeg_62], \
-    eeg_63=[eeg_63], eeg_64=[eeg_64], \
-    ecg_x=[ecg_x], ecg_y=[ecg_y]), rollover=750)
+    eeg_63=[eeg_63], eeg_64=[eeg_64]), rollover=1)
+    
+    eeg.visualize(i_eeg, source_eeg)
 
+@gen.coroutine
+def update_ecg(ecg_x, ecg_y):
+
+    source_ecg.stream(dict(ecg_x=[ecg_x], ecg_y=[ecg_y]), rollover=750)
+    
 @gen.coroutine
 def update_num(num_x, num_y, spo2, gsr, hrv, rr, hr_x, hr_y, hr):
 
@@ -85,23 +93,23 @@ def update_num(num_x, num_y, spo2, gsr, hrv, rr, hr_x, hr_y, hr):
     spo2=[spo2], gsr=[gsr], hrv=[hrv], rr=[rr], \
     hr_x=[hr_x], hr_y=[hr_y], hr=[hr]), rollover=1)
 
-@gen.coroutine
-def update_eeg(i, source):
-    eeg.visualize(i, source)
     
 def blocking_task():
     while True:
         global i
+        global i_eeg
         # do some blocking computation
 
         # pause for 0.01 seconds
         time.sleep(0.01)
         # update x index
         i = i+1
+        
+        i_eeg = i_eeg + 1
 
         # get data from inlet
         sample, timestamp = inlet.pull_sample()
-
+        
         # append eeg data
         eeg_1, eeg_2, eeg_3, eeg_4, \
         eeg_5, eeg_6, eeg_7, eeg_8, eeg_9, eeg_10,\
@@ -113,16 +121,16 @@ def blocking_task():
         eeg_41, eeg_42, eeg_43, eeg_44, eeg_45, eeg_46,\
         eeg_47, eeg_48, eeg_49, eeg_50, eeg_51, eeg_52,\
         eeg_53, eeg_54, eeg_55, eeg_56, eeg_57, eeg_58,\
-        eeg_59, eeg_60, eeg_61, eeg_62, eeg_63, eeg_64 = sample[0:64]
-
+        eeg_59, eeg_60, eeg_61, eeg_62, eeg_63, eeg_64 = sample[:64]
+        
         # append ecg data
         ecg_x, ecg_y = i, -sample[68]+6000
 
         # define position and value for hr
-        hr_x = i-24
+        hr_x, hr_y = i-24, 3800
         if i<25:
             hr_x=1
-        hr_y = 3800
+        # define text for hr display
         hr = 'HR=' + str(round(sample[72]))
 
         # compute currnt hrv
@@ -139,9 +147,10 @@ def blocking_task():
         gsr = str(round(sample[73]))
         hrv = str(round(measurements['rmssd']))
         rr = str(round(sample[69]))
-
+        
+        
         # but update the document from callback
-        doc.add_next_tick_callback(partial(update, eeg_1=eeg_1, eeg_2=eeg_2, \
+        doc.add_next_tick_callback(partial(update_eeg, eeg_1=eeg_1, eeg_2=eeg_2, \
         eeg_3=eeg_3, eeg_4=eeg_4, eeg_5=eeg_5, eeg_6=eeg_6, eeg_7=eeg_7, \
         eeg_8=eeg_8, eeg_9=eeg_9, eeg_10=eeg_10,eeg_11=eeg_11, eeg_12=eeg_12, \
         eeg_13=eeg_13, eeg_14=eeg_14, eeg_15=eeg_15, eeg_16=eeg_16, eeg_17=eeg_17, \
@@ -154,21 +163,20 @@ def blocking_task():
         eeg_48=eeg_48, eeg_49=eeg_49, eeg_50=eeg_50, eeg_51=eeg_51, eeg_52=eeg_52,\
         eeg_53=eeg_53, eeg_54=eeg_54, eeg_55=eeg_55, eeg_56=eeg_56, eeg_57=eeg_57, \
         eeg_58=eeg_58, eeg_59=eeg_59, eeg_60=eeg_60, eeg_61=eeg_61, eeg_62=eeg_62, \
-        eeg_63=eeg_63, eeg_64=eeg_64, \
-        ecg_x=ecg_x, ecg_y=ecg_y))
+        eeg_63=eeg_63, eeg_64=eeg_64))
 
-        # but update the document from callback
+        # update ecg
+        doc.add_next_tick_callback(partial(update_ecg, ecg_x=ecg_x, ecg_y=ecg_y))
+
+        # update numbers
         doc.add_next_tick_callback(partial(update_num, num_x=num_x, num_y=num_y, \
         spo2=spo2, gsr=gsr, hrv=hrv, rr=rr, hr_x=hr_x, hr_y=hr_y, hr=hr))
         
-        # but update the document from callback
-        doc.add_next_tick_callback(partial(update_eeg, i=i, source=source))
-
-
+        
 # DEFINE VISUALIZATION MODULES
 # ------------------------------------------------------------------------------
 eeg = eegModule()
-ecg = ecgModule(source, source_num)
+ecg = ecgModule(source_ecg, source_num)
 spo2 = spo2Module(source_num)
 gsr = gsrModule(source_num)
 hrv = hrvModule(source_num)
@@ -177,7 +185,7 @@ resp = respModule(source_num)
 # CREATE LAYOUT
 # ------------------------------------------------------------------------------
 p = column(row(eeg.DeltaFig, eeg.ThetaFig, eeg.AlphaFig, eeg.TotalFig), \
-           row(ecg.Fig, column(hrv.Fig, resp.Fig, spo2.Fig, gsr.Fig)))
+           row(ecg.Fig, Spacer(width=125), column(hrv.Fig, resp.Fig, spo2.Fig, gsr.Fig)))
 
 # MAKE PLOT THE ROOT OF DOCUMENT
 # ------------------------------------------------------------------------------
@@ -188,7 +196,6 @@ doc.add_root(p)
 # ------------------------------------------------------------------------------
 # create thread that calls the target
 thread = Thread(target=blocking_task)
-# start thread
 thread.start()
 
 
