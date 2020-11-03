6e9c75414276e3c4d725251f1aedda67e65da5af
rohitkshenoy <36802469+rohitkshenoy@users.noreply.github.com>
2020-11-02 19:56:34 -0700

    Updated bokehNumber.py to include tooltip

--- app/modules/bokehNumber.py
+++ app/modules/bokehNumber.py
@@ -2,7 +2,7 @@ import numpy as np
 import random as random
 from bokeh.io import curdoc, show
 from bokeh.io.state import *
-from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Text
+from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Text, HoverTool
 from bokeh.core import enums
 from bokeh.core.properties import Enum
 from pylsl import StreamInlet, resolve_stream
@@ -26,6 +26,13 @@ def add_text():
     source = ColumnDataSource(dict(x=x, y=y, text=text))
     glyph = Text(x="x", y="y", text="text", text_font_size="100px", text_align="center", text_baseline="middle", angle=0, text_color="#96deb3")
     plot.add_glyph(source, glyph)
+    plot.add_tools(HoverTool(
+        tooltips=[
+            ("GSR","The galvanic skin response refers to changes in sweat gland activity that are reflective of the intensity of our emotional state"),
+            ("SPO2", "Oxygen saturation is a measurement of how much oxygen your blood is carrying as a percentage of the maximum it could carry. For a healthy individual, it should be between 96% to 99%")
+        ]
+        #,mode='vline'
+    ))
     print("Printed " + str(text[0]))
 
 plot = Plot(
